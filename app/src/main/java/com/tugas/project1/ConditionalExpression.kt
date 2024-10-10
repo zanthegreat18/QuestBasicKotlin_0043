8a8348e9a37256e6744b4b9eb89c53d3ec2be2bc
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 02:41:48 +0700

    commit 5

---
+++ app/src/main/java/com/tugas/project1/ConditionalExpression.kt
@@ -0,0 +1,16 @@
+package com.tugas.project1
+
+// Conditional Expression
+// Condotional expression adalah expression yang memiliki nilai true atau false
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+    // if-else
+    val number = 10
+    if (number > 0) {
+        println("Angka positif")
+    } else {
+        println("Angka negatif")
+    }
+}
