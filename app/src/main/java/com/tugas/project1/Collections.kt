2ffdb95420f2cbf26bed95a70456c9c6753fb722
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 02:29:56 +0700

    commit 3

--- app/src/main/java/com/tugas/project1/Collections.kt
+++ app/src/main/java/com/tugas/project1/Collections.kt
@@ -61,4 +61,39 @@ val shapesLocked: Set<String> = shape
 println(shapesLocked)
 }
 
+// Map adalah kumpulan data yang memiliki pasangan key dan value
+// Map bersifat unordered, artinya data yang dimasukan tidak memiliki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+
+println()
+println("=== Map ===")
+
+// Map Read-Only
+val readOnlyShape: Map<String, Int> = mapOf("Lingkaran" to 1, "Segitiga" to 2, "Persegi" to 3)
+println(readOnlyShape)
+
+// Map Mutable
+val shape: MutableMap<String, Int> = mutableMapOf("Lingkaran" to 1, "Segitiga" to 2, "Persegi" to 3)
+println(shape)
+
+// Menambahkan data ke map mutable
+shape["Rectangle"] = 4
+println(shape)
+
+// Menghapus data dari map mutable
+shape.remove("Segitiga")
+println(shape)
+
+// Mengubah data dari map mutable
+shape["Persegi"] = 5
+println(shape)
+
+// Map Read-Only
+val shapesLocked: Map<String, Int> = shape
+println(shapesLocked)
+}
 
