6c12a338d07bf69ccc0f1844cdc6930368b2184d
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 13:08:54 +0700

    commit 6 when

--- app/src/main/java/com/tugas/project1/ConditionalExpression.kt
+++ app/src/main/java/com/tugas/project1/ConditionalExpression.kt
@@ -13,4 +13,26 @@ fun ConditionalStatement() {
     } else {
         println("Angka negatif")
     }
+
+    // when
+    val day = 1
+    when (day) {
+        1 -> println("Senin")
+        2 -> println("Selasa")
+        3 -> println("Rabu")
+        4 -> println("Kamis")
+        5 -> println("Jumat")
+        6 -> println("Sabtu")
+        7 -> println("Minggu")
+        else -> println("Tidak valid")
+    }
+
+    // Try-catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
 }
