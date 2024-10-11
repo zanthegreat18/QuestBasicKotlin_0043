18c93beb4c1f0e0076f208933ca51641e8904550
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 13:32:06 +0700

    commit 9 Fungctions

---
+++ app/src/main/java/com/tugas/project1/Fungctions.kt
@@ -0,0 +1,11 @@
+package com.tugas.project1
+
+fun withoutParameter() {
+println("=== Without Parameter ===")
+println("Hello World")
+}
+
+fun withParameter(name: String) {
+println("=== With Parameter ===")
+println("Hello $name")
+}
