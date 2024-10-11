22ffeba8672bd145c94c123ae947acc76308d190
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 13:18:38 +0700

    commit ConditionalExpression.kt

--- app/src/main/java/com/tugas/project1/ConditionalExpression.kt
+++ app/src/main/java/com/tugas/project1/ConditionalExpression.kt
@@ -36,3 +36,33 @@ fun ConditionalStatement() {
         println("Invalid Number")
     }
 }
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+for (i in 1..5) {
+    println("Perulangan ke-$i")
+}
+
+val cars = listOf("Toyota", "Honda", "Ford")
+for (car in cars) {
+println("car type: $car")
+}
+
+var x = 1
+while (x <= 5) {
+    println("Perulangan ke-$x")
+    x++
+}
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
+}
