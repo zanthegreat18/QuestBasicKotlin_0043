e604d5dfb80d54b020d454f9e003127c2a3f1595
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 13:35:15 +0700

    commit 10 Named Argument and Default paramter

--- app/src/main/java/com/tugas/project1/Fungctions.kt
+++ app/src/main/java/com/tugas/project1/Fungctions.kt
@@ -9,3 +9,20 @@ fun withParameter(name: String) {
 println("=== With Parameter ===")
 println("Hello $name")
 }
+
+// Named argument
+// Named argument adalah fitur yang memungkinkan kita untuk menentukan parameter dengan nama yang akan dikirimkan ke fungsi
+fun withNamedArgument(name: String, age: Int) {
+println()
+println("== withNamedArgument ==")
+println("Hello $name, your $age years old")
+}
+
+// Default parameter value
+// Default parameter value adalah fitur yang memungkinkan kita untuk menentukan nilai default dari parameter
+fun withDefaultParameter(name: String = "zan", age: Int = 20) {
+println()
+println("== withDefaultParameter ==")
+println("Hello $name, your $age years old")
+}
+
