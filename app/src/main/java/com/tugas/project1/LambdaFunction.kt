ca4366ade5197006d8f46fe9bca6464bf426a488
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-11 13:47:55 +0700

    commit 12 Lambda

---
+++ app/src/main/java/com/tugas/project1/LambdaFunction.kt
@@ -0,0 +1,11 @@
+package com.tugas.project1
+
+// Lambda Functions
+// Lambda functions adalah fungsi yang tidak memiliki nama
+// Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
